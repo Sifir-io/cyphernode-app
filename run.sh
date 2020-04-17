a5a8eb4c6b2564f7265e8d95c3f4662ea224954a
gabidi-c1 <gabidi@gmail.com>
2020-04-17 11:48:20 -0400

    fix run.sh

--- run.sh
+++ run.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-cn_cert_filefullpath="${1:-'./cacert.pem'}"
+cn_cert_filefullpath=${1:-'./cacert.pem'}
 if [ ! -f "$cn_cert_filefullpath" ]; then
 	echo "Missing Cyphernode cert filefull path as first argument";
-	echo "You must provide the path to Cyphernode's certificate file as an argument to the run script, ex: \n ./ruh.sh ./cyphernode/dist/cyphernode/certs/cert.pem \n"
+	echo "You must provide the path to Cyphernode's certificate file as an argument to the run script, ex:"
+ 	echo "./ruh.sh ./cyphernode/dist/cyphernode/certs/cert.pem"
 	exit -1
 fi;
 CYPHERNODE_GATEKEEPER_CERT_CA=$(cat "$cn_cert_filefullpath") docker-compose --env-file ./.env up ${2} 
